e8cddfd063fc9ccd5032ccb466507c1784e8fc0d
Nick <Nbarnes0521@gmail.com>
2023-05-11 19:46:02 -0500

    Helper Functions pt.1

--- db/index.js
+++ db/index.js
@@ -4,3 +4,12 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 module.exports = {
   client,
 }
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
--- db/seed.js
+++ db/seed.js
@@ -2,14 +2,17 @@ const { client } = require('./index');
 
 async function testDB() {
     try {
-        client.connect();
-        const result = await client.query(`SELECT * FROM users;`);
-        console.log(result);
+      client.connect();
+  
+      const { rows } = await client.query(`SELECT * FROM users;`);
+      console.log(rows);
     } catch (error) {
       console.error(error);
     } finally {
-        client.end();
+      client.end();
     }
   }
   
   testDB();
+
+  
