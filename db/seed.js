51a84770fa0c0ff9cde7dfb08acb31f775132e9b
Nick <Nbarnes0521@gmail.com>
2023-05-11 20:15:05 -0500

    Seeding part1

--- db/seed.js
+++ db/seed.js
@@ -1,19 +1,60 @@
-const {
-    client,
-    getAllUsers // new
-  } = require('./index');
+const { client } = require('./index');
 
-  async function testDB() {
-    try {
-      client.connect();
-  
-      const users = await getAllUsers();
-      console.log(users);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      client.end();
-    }
+// Define the getAllUsers function
+async function getAllUsers() {
+  try {
+    // Query to retrieve all users goes here
+  } catch (error) {
+    throw error; 
   }
-  
-  testDB();
+}
+
+async function testDB() {
+  try {
+    await client.connect();
+
+    const users = await getAllUsers();
+    console.log(users);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+async function dropTables() {
+  try {
+    await client.query(`
+      // Query to drop tables goes here
+    `);
+  } catch (error) {
+    throw error; 
+  }
+}
+
+async function createTables() {
+  try {
+    await client.query(`
+      // Query to create tables goes here
+    `);
+  } catch (error) {
+    throw error; 
+  }
+}
+
+async function rebuildDB() {
+  try {
+    await client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+}
+
+
+testDB();
+rebuildDB();
